722784a4bc5cf855683483db29944fa4939604a5
Nils Asmussen <nils@os.inf.tu-dresden.de>
2022-04-18 11:40:34 +0200

    Added support for lstat.
    We don't support symlinks, therefore lstat is the same as fstat.

--- m3/syscall.cc
+++ m3/syscall.cc
@@ -81,6 +81,9 @@ static const char *syscall_name(long no) {
 #if defined(SYS_fstatat)
         case SYS_fstatat:           return "stat";
 #endif
+#if defined(SYS_lstat)
+        case SYS_lstat:             return "lstat";
+#endif
         case SYS_getdents64:        return "getdents64";
 #if defined(SYS_mkdir)
         case SYS_mkdir:
@@ -275,6 +278,9 @@ EXTERN_C long __syscall6(long n, long a, long b, long c, long d, long e, long f)
 #endif
 #if defined(SYS_stat)
         case SYS_stat:              res = __m3_fstatat(-1, (const char*)a, (struct kstat*)b, 0); break;
+#endif
+#if defined(SYS_lstat)
+        case SYS_lstat:             res = __m3_fstatat(-1, (const char*)a, (struct kstat*)b, 0); break;
 #endif
 #if defined(SYS_stat64)
       case SYS_stat64:              res = __m3_fstatat(-1, (const char*)a, (struct kstat*)b, 0); break;
