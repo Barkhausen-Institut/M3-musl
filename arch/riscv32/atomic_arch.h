8a9108852be2c07b79e203b515246967a2419d85
Nils Asmussen <nils@os.inf.tu-dresden.de>
2024-05-24 11:08:55 +0200

    riscv32: don't use fence on hw (PicoRV32 core).

--- arch/riscv32/atomic_arch.h
+++ arch/riscv32/atomic_arch.h
@@ -1,12 +1,12 @@
 #define a_barrier a_barrier
-static inline void a_barrier()
-{
-	__asm__ __volatile__ ("fence rw,rw" : : : "memory");
+static inline void a_barrier() {
+#if defined(__gem5__)
+    __asm__ __volatile__("fence rw,rw" : : : "memory");
+#endif
 }
 
 #define a_cas a_cas
-static inline int a_cas(volatile int *p, int t, int s)
-{
+static inline int a_cas(volatile int *p, int t, int s) {
     int old = *p;
     if(old == t)
         *p = s;
